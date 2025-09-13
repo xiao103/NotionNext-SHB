9995c24a5cf27e0ccee35b0c4c288760f53b99c6
xiao103 <zhuce3@icloud.com>
2025-09-13 13:04:42 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -30,11 +30,13 @@ const CONFIG = {
   HEO_HERO_CATEGORY_1: {title:'行业资讯',url:'/tag/文章'},HEO_HERO_CATEGORY_2: {title:'学具产品',url:'/tag/学具'},HEO_HERO_CATEGORY_3: {title:'教具产品',url:'/tag/教具'},
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '初中',HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false， // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  HEO_HERO_RECOMMEND_POST_TAG: '初中',
+  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false ， // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
-   HEO_INFOCARD_GREETINGS: [
+  
+  HEO_INFOCARD_GREETINGS:[
     '你好！我是',
     '生本教具'
   ],

c5d3aa23bae953074d880bd2d453036b4e53dc5c
xiao103 <zhuce3@icloud.com>
2025-09-13 11:23:58 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
-  HEO_HOME_POST_TWO_COLS: true, // 首页博客两列显示，若为false则只显示一列
+  HEO_HOME_POST_TWO_COLS: true, // 首页博客两列显示,若为false则只显示一列
   HEO_LOADING_COVER: true, // 页面加载的遮罩动画
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2019-01-01', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2019-01-01', // 建站日期,用于计算网站运行的第几天
 
-  // 首页顶部通知条滚动内容，如不需要可以留空 []
+  // 首页顶部通知条滚动内容,如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '一切以学生为根本！', url: 'https://blog.x1ao.shop'  }
   ],
@@ -30,7 +30,7 @@ const CONFIG = {
   HEO_HERO_CATEGORY_1: {title:'行业资讯',url:'/tag/文章'},HEO_HERO_CATEGORY_2: {title:'学具产品',url:'/tag/学具'},HEO_HERO_CATEGORY_3: {title:'教具产品',url:'/tag/教具'},
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '初中',HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  HEO_HERO_RECOMMEND_POST_TAG: '初中',HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false， // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
@@ -50,7 +50,7 @@ const CONFIG = {
   // 用户技能图标
   
 
-  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD: false， // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
@@ -63,30 +63,31 @@ const CONFIG = {
   HEO_SITE_VISITOR_TITLE: '访客数:',
 
   // *****  以下配置无效，只是预留开发 ****
+  
   // 菜单配置
-  HEO_MENU_INDEX: true, // 显示首页
-  HEO_MENU_CATEGORY: true, // 显示分类
+  HEO_MENU_INDEX: true， // 显示首页
+  HEO_MENU_CATEGORY: true， // 显示分类
   HEO_MENU_TAG: true， // 显示标签
-  HEO_MENU_ARCHIVE: true, // 显示归档
-  HEO_MENU_SEARCH: false, // 显示搜索
+  HEO_MENU_ARCHIVE: true， // 显示归档
+  HEO_MENU_SEARCH: false， // 显示搜索
 
-  HEO_POST_LIST_COVER: true, // 列表显示文章封面
-  HEO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
+  HEO_POST_LIST_COVER: true， // 列表显示文章封面
+  HEO_POST_LIST_COVER_HOVER_ENLARGE: false， // 列表鼠标悬停放大
 
-  HEO_POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
-  HEO_POST_LIST_SUMMARY: true, // 文章摘要
-  HEO_POST_LIST_PREVIEW: false, // 读取文章预览
-  HEO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
+  HEO_POST_LIST_COVER_DEFAULT: true， // 封面为空时用站点背景做默认封面
+  HEO_POST_LIST_SUMMARY: true， // 文章摘要
+  HEO_POST_LIST_PREVIEW: false， // 读取文章预览
+  HEO_POST_LIST_IMG_CROSSOVER: true， // 博客列表图片左右交错
 
-  HEO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  HEO_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
-  HEO_ARTICLE_RECOMMEND: true, // 文章关联推荐
+  HEO_ARTICLE_ADJACENT: true， // 显示上一篇下一篇文章推荐
+  HEO_ARTICLE_COPYRIGHT: true， // 显示文章版权声明
+  HEO_ARTICLE_RECOMMEND: true， // 文章关联推荐
 
-  HEO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
-  HEO_WIDGET_ANALYTICS: false, // 显示统计卡
+  HEO_WIDGET_LATEST_POSTS: true， // 显示最新文章卡
+  HEO_WIDGET_ANALYTICS: false， // 显示统计卡
   HEO_WIDGET_TO_TOP: true,
-  HEO_WIDGET_TO_COMMENT: true, // 跳到评论区
-  HEO_WIDGET_DARK_MODE: true, // 夜间模式
+  HEO_WIDGET_TO_COMMENT: true， // 跳到评论区
+  HEO_WIDGET_DARK_MODE: true， // 夜间模式
   HEO_WIDGET_TOC: true // 移动端悬浮目录
 }
 export default CONFIG
